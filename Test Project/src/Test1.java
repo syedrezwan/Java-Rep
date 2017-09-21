8fa3460664019431926bff89a8c3d5a29d59a870
syedrezwan <syed@easysolutions.in>
2017-09-21 15:41:31 +0530

    commit & push

--- Test Project/src/Test1.java
+++ Test Project/src/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println( "Welcome" );
 		System.out.println( "test 1" );
-
+		System.out.println( "push and commit" );
 	}
 
 }
